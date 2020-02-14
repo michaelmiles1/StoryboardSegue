9b7fe716883e60bcad5a29fb388092a7bfd99999
michaelmiles1 <michael.gage.miles@gmail.com>
2020-02-14 16:03:10 -0500

    Initial Commit

---
+++ StoryboardSegue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryboardSegue
+//
+//  Created by Michael Miles on 2/14/20.
+//  Copyright © 2020 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
